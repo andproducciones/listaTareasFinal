3fbd9e0abfae878d9a7f37066c585262cd04d3a6
Francis Andree Fiallos Silva <fafs.1405@gmail.com>
2025-01-28 23:40:21 -0500

    2

--- src/app/pages/task-form/task-form.page.ts
+++ src/app/pages/task-form/task-form.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-task-form',
@@ -7,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class TaskFormPage implements OnInit {
+  task = { titulo: '', descripcion: '', estado: 'pendiente' };
+  taskId: number | null = null;
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute,
+    private navCtrl: NavController
+  ) {}
 
   ngOnInit() {
+    this.taskId = this.route.snapshot.params['id'];
+    if (this.taskId) {
+      this.taskService.getTaskById(this.taskId).subscribe(response => {
+        if (response.estado) {
+          this.task = response.tarea;
+        }
+      });
+    }
   }
 
+  saveTask() {
+    if (this.taskId) {
+      this.taskService.updateTask({ id: this.taskId, ...this.task }).subscribe(() => this.navCtrl.navigateBack('/task-list'));
+    } else {
+      this.taskService.addTask(this.task).subscribe(() => this.navCtrl.navigateBack('/task-list'));
+    }
+  }
 }
