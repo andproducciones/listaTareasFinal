d7036f995bbdf20bc008e16122fbbcebef7f7e65
Francis Andree Fiallos Silva <fafs.1405@gmail.com>
2025-01-29 00:55:23 -0500

    444

--- src/app/pages/task-detail/task-detail.module.ts
+++ src/app/pages/task-detail/task-detail.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
-import { TaskDetailPageRoutingModule } from './task-detail-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 
 import { TaskDetailPage } from './task-detail.page';
-import { RouterModule } from '@angular/router';
+
+const routes: Routes = [  // ✅ Definimos correctamente la constante `routes`
+  { path: '', component: TaskDetailPage }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    TaskDetailPageRoutingModule,
-    RouterModule
+    RouterModule.forChild(routes)  // ✅ Aquí usamos `routes`, no `Routes`
   ],
   declarations: [TaskDetailPage]
 })
