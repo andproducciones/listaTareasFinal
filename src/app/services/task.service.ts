b5c561e9e11ca8650f8003a33f8774a3a2b35c5d
Francis Andree Fiallos Silva <fafs.1405@gmail.com>
2025-01-28 23:18:04 -0500

    Servicio

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private API_URL = 'https://luchandovenceras.online/backend/listatareas.php';
 
-  constructor() { }
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    })
+  };
+
+  constructor(private http: HttpClient) {}
+
+  // Obtener todas las tareas
+  getTasks(): Observable<any> {
+    return this.http.post(this.API_URL, { action: 'listar' });
+  }
+
+  // Obtener una tarea por ID
+  getTaskById(id: number): Observable<any> {
+    return this.http.post(this.API_URL, { action: 'tarea', id });
+  }
+
+  // Agregar una nueva tarea
+  addTask(task: any): Observable<any> {
+    return this.http.post(this.API_URL, { action: 'insertar', ...task });
+  }
+
+  // Actualizar una tarea existente
+  updateTask(task: any): Observable<any> {
+    return this.http.post(this.API_URL, { action: 'actualizar', ...task });
+  }
+
+  // Eliminar una tarea por ID
+  deleteTask(id: number): Observable<any> {
+    return this.http.post(this.API_URL, { action: 'eliminar', id });
+  }
 }
